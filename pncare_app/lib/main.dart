8d2631a1de2c76b959e40a3b7b161911b28655be
bigsas777 <75082021+bigsas777@users.noreply.github.com>
2021-05-20 22:35:37 +0200

    PnCare_v1.0.4
    sos

--- pncare_app/lib/main.dart
+++ pncare_app/lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
-import 'package:splashscreen/splashscreen.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 import 'package:pncare_app/autenticazione/sign_in.dart';
@@ -30,16 +28,14 @@ class PnCare extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-        splash: Image.asset('assets/immagini/splashScreen.gif'),
-        nextScreen: Home(),
-        splashIconSize: 700,
-        // splashTransition: SplashTransition.sizeTransition,
-        backgroundColor: Color(0xfff5e9be),
-        duration: 1000,
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: AnimatedSplashScreen(
+          splash: Image.asset('assets/immagini/logo_trasparente_main.png'),
+          nextScreen: Home(),
+          splashIconSize: 700,
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Color(0xfff5e9be),
+          duration: 1000,
+        ));
   }
 }
-
